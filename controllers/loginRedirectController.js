f1bfa6d133396e788357e99e64d6a87fae1ca311
Nghia Nguyen <AlexNguyen@Nghias-MBP.attlocal.net>
2016-03-06 23:00:17 -0600

    Hw7

---
+++ controllers/loginRedirectController.js
@@ -0,0 +1,11 @@
+var app = angular.module('home', ["firebase"]);
+ var ref = new Firebase("https://projectipcservice.firebaseio.com/");
+  var authData = ref.getAuth();
+app.controller('loginRedirect', function($scope, $firebaseArray) 
+{
+   
+   if(!authData)
+    {
+        window.location.href = "index.html";
+    }
+});
